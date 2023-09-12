b3daa9b19f90e5f0545f08d06ff1769295ca6af1
Shakh <waxboz.nematov@gmail.com>
2023-09-12 09:14:58 +0500

    setup: swagger for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setDescription('API Documentation')
+    .setVersion('1.0.0')
+    .addTag('tm')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
 }
+
 bootstrap();
